16f2905fdd62b33e8888fb21b5a57907cdb14bb1
Eric C. Anderson <eric.anderson@noaa.gov>
2015-04-30 15:38:16 -0700

    put a build script in to circumvent the Autotools madness...

---
+++ build_script.sh
@@ -0,0 +1,18 @@
+CFLAGS="-I./eca-shared/ecalibs \
+	 -I./eca-shared/ranlib/src"
+
+
+gsisim_SOURCES="./eca-shared/ranlib/linpack/linpack.c \
+    ./eca-shared/ranlib/src/com.c \
+    ./eca-shared/ranlib/src/ranlib.c \
+    ./eca-shared/ecalibs/ECA_MemAlloc.c \
+    ./eca-shared/ecalibs/ECA_Opt3.c \
+    ./eca-shared/ecalibs/ECA_ReadData.c \
+    ./eca-shared/ecalibs/ECA_utilities.c \
+    ./eca-shared/ecalibs/MathStatRand.c \
+    ./eca-shared/ecalibs/MCTypesEtc.c \
+    ./src/gsi_sim.c"
+
+
+gcc -O3 $CFLAGS  $gsisim_SOURCES -o gsisim -lm
+
